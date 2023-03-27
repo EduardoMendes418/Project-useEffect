c357938547c7bf1cacf8a4fc215d904d6e7aa6df
Eduardo Mendes <eduardo.mendes@sensedia.com>
2023-03-27 07:40:09 -0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const eventFn = () => {
+   console.log('h1 clicado');
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [counter, setCounter] = useState(0);
+   const [counter2, setCounter2] = useState(0);
+
+   //componentDidUpdate atualizada toda vez que é chamado
+   // useEffect(() => {
+   //    console.log('componentDidUpdate');
+   // });
+
+   // componentDidMount executa somente uma vez
+   useEffect(() => {
+      document.querySelector('h1')?.addEventListener('click', eventFn);
+
+      //compoentWillUmount - limpeza
+      return () => {
+         document.querySelector('h1')?.removeEventListener('click', eventFn);
+      };
+   }, []);
+
+   //execulta toda vez que independecia muda
+   useEffect(() => {
+      console.log('C1:', { counter }, 'C2:', { counter2 });
+   }, [counter, counter2]);
+
+   return (
+      <div className="App">
+         <h1>
+            C1:{counter} C2:{counter2}
+         </h1>
+         <button onClick={() => setCounter(counter + 1)}>+</button>
+         <button onClick={() => setCounter2(counter2 + 1)}>+(2)</button>
+      </div>
+   );
 }
 
 export default App;
